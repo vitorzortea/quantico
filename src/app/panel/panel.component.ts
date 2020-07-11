6f683f403e14938b8d8a2e71c59801d7f08a69a0
Vitor Zortéa <vitorzortea@gmail.com>
2020-07-11 12:25:02 -0300

    Stylus Bas

---
+++ src/app/panel/panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.styl']
+})
+export class PanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
